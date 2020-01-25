2e0588067f649b4bb7e9a616774994a1137f0af8
Pekka Enberg <penberg@iki.fi>
2020-01-25 08:36:42 +0200

    install-deps.sh for installing dependencies

---
+++ install-deps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. /etc/os-release
+
+if [ "$ID" = "fedora" ]; then
+  dnf install make cmake gcc-c++ zlib-devel python3 hwloc-devel
+elif [ "$ID" = "ubuntu" ]; then
+  apt install make cmake gcc zlib1g-dev python3 libhwloc-dev
+else
+  echo "$0: $ID is not a supported OS."
+fi
