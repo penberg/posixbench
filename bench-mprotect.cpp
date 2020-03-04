3e73bc02e5f2493bd930ebb05a72e4c048226566
Pekka Enberg <penberg@iki.fi>
2020-03-04 20:48:47 +0200

    bench-mprotect: Measure latency of one mprotect() call

--- bench-mprotect.cpp
+++ bench-mprotect.cpp
@@ -2,20 +2,42 @@
 
 #include <sys/mman.h>
 
-static void *map;
 static constexpr size_t size = 1024 * 1024; /* 1 MB */
 
-struct Op {
-  void operator()() {
+struct Action {
+  void *map;
+
+  void init() {
+    map = ::mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    assert(map != MAP_FAILED);
+  }
+
+  void release() {
+    ::munmap(map, size);
+  }
+
+  uint64_t measured_operation() {
+    struct timespec start, end;
+    if (clock_gettime(CLOCK_MONOTONIC, &start) < 0) {
+      assert(0);
+    }
+    ::mprotect(map, size, PROT_NONE);
+    if (clock_gettime(CLOCK_MONOTONIC, &end) < 0) {
+      assert(0);
+    }
+    ::mprotect(map, size, PROT_READ | PROT_WRITE);
+    uint64_t start_ns = timespec_to_ns(&start);
+    uint64_t end_ns = timespec_to_ns(&end);
+    return end_ns - start_ns;
+  }
+
+  void other_operation(size_t tid) {
     ::mprotect(map, size, PROT_NONE);
     ::mprotect(map, size, PROT_READ | PROT_WRITE);
   }
 };
 
+
 int main(int argc, char *argv[]) {
-  map = ::mmap(nullptr, size, PROT_READ | PROT_WRITE,
-               MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-  assert(map != MAP_FAILED);
-  run_all<SymmetricAction<Op>>(argc, argv);
-  ::munmap(map, size);
+  run_all<Action>(argc, argv);
 }
