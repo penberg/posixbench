08c9edb09c31ce4c4d79c3c88ae04b8d30101df5
Pekka Enberg <penberg@iki.fi>
2020-03-04 21:01:17 +0200

    plot-latency: Add reference lines for L3 cache and Optane
    Let's add L3 cache access time and Optane random load time as reference
    to the plots to highlight the magnitude of time for each measured POSIX
    operation.

--- scripts/plot-latency.py
+++ scripts/plot-latency.py
@@ -20,11 +20,24 @@ plt.style.use('seaborn-ticks')
 
 color = itertools.cycle(mpl.rcParams['axes.prop_cycle'].by_key()['color'])
 
+# Intel Xeon L3 access latency
+# (Source: https://www.anandtech.com/show/8423/intel-xeon-e5-version-3-up-to-18-haswell-ep-cores-/11)
+L3_access_latency = 0.020 # microseconds 
+
+# Intel Optane random load
+# (Source: https://arxiv.org/pdf/1903.05714.pdf)
+optane_latency = 0.305 # microseconds
+
 fig, ax = plt.subplots()
 for name, group in df.groupby('scenario'):
   group.plot(x='time', y='percentile', legend=True, label=name, ax=ax)
+
+plt.axvline(x=L3_access_latency, label="L3 cache", color="black", linestyle="dotted")
+plt.axvline(x=optane_latency, label="Optane (random load)", color="black", linestyle="dashed")
+
 plt.xlabel('Time (μs)')
 plt.ylabel('Percentile (%)')
+plt.legend(loc='lower right', frameon=True, framealpha=1)
 
 prefix, _ = os.path.splitext(args.filename)
 plt.savefig("%s.pdf" % (prefix), format='pdf')
