094cc9bdf2dc1126bdff813becbca86838f16884
Pekka Enberg <penberg@iki.fi>
2020-05-19 16:56:42 +0300

    bench-pthread-kill: Fix latency measurement

--- bench-pthread-kill.cpp
+++ bench-pthread-kill.cpp
@@ -2,18 +2,13 @@
 
 #include <sys/eventfd.h>
 
+#include <deque>
 #include <vector>
 
 static std::vector<uint64_t> remote_timestamps;
+static std::deque<bool> signaled; /* protected by "mutexes" */
 static std::vector<pthread_cond_t> cond_vars;
 static std::vector<pthread_mutex_t> mutexes;
-
-static void signal_handler(int signum) {
-    struct timespec now;
-    if (clock_gettime(CLOCK_MONOTONIC, &now) < 0) {
-      assert(0);
-    }
-}
 
 struct State {
 };
@@ -28,17 +23,13 @@ struct Action {
   }
 
   benchmark::NoState make_state(const benchmark::ThreadVector& ts) {
-    struct ::sigaction sa;
-    sa.sa_handler = signal_handler;
-    sa.sa_flags = 0;
-    ::sigemptyset(&sa.sa_mask);
-    ::sigaction(SIGUSR1, &sa, nullptr);
     return benchmark::NoState(ts);
   }
 
   void raw_operation(benchmark::NoState& state) {
     auto other_thread = const_cast<std::thread&>(state.interfering_threads[remote_tid]).native_handle();
     ::pthread_kill(other_thread, SIGUSR1);
+    ::pthread_cond_wait(&cond_vars[remote_tid], &mutexes[remote_tid]);
     remote_tid = (remote_tid + 1) % remote_timestamps.size();
   }
 
@@ -49,7 +40,12 @@ struct Action {
       assert(0);
     }
     ::pthread_kill(other_thread, SIGUSR1);
-    ::pthread_cond_wait(&cond_vars[remote_tid], &mutexes[remote_tid]);
+    ::pthread_mutex_lock(&mutexes[remote_tid]);
+    while (!signaled[remote_tid]) {
+      ::pthread_cond_wait(&cond_vars[remote_tid], &mutexes[remote_tid]);
+    }
+    signaled[remote_tid] = false;
+    ::pthread_mutex_unlock(&mutexes[remote_tid]);
     uint64_t start_ns = benchmark::timespec_to_ns(&start);
     uint64_t end_ns = remote_timestamps[remote_tid];
     remote_tid = (remote_tid + 1) % remote_timestamps.size();
@@ -57,12 +53,22 @@ struct Action {
   }
 
   void other_operation(benchmark::NoState& state, size_t tid) {
-    struct timespec end;
-    if (clock_gettime(CLOCK_MONOTONIC, &end) < 0) {
+    ::sigset_t set;
+    ::sigemptyset(&set);
+    ::sigaddset(&set, SIGUSR1);
+    ::sigaddset(&set, SIGINT);
+    int sig;
+    ::sigwait(&set, &sig);
+    struct timespec now;
+    if (clock_gettime(CLOCK_MONOTONIC, &now) < 0) {
       assert(0);
     }
-    remote_timestamps[tid] = benchmark::timespec_to_ns(&end);
+    assert(sig == SIGUSR1 || sig == SIGINT);
+    remote_timestamps[tid] = benchmark::timespec_to_ns(&now);
+    ::pthread_mutex_lock(&mutexes[tid]);
+    signaled[tid] = true;
     ::pthread_cond_signal(&cond_vars[tid]);
+    ::pthread_mutex_unlock(&mutexes[tid]);
   }
 
   bool supports_non_interference() { return false; }
@@ -73,11 +79,15 @@ struct Action {
 static void init(size_t nr_threads) {
   for (unsigned i = 0; i < nr_threads; i++) {
     remote_timestamps.push_back(0);
+    signaled.push_back(false);
     cond_vars.push_back(PTHREAD_COND_INITIALIZER);
     mutexes.push_back(PTHREAD_MUTEX_INITIALIZER);
   }
 }
 
 int main(int argc, char *argv[]) {
+  ::sigset_t all_sigs;
+  ::sigfillset(&all_sigs);
+  assert(::sigprocmask(SIG_SETMASK, &all_sigs, nullptr) == 0);
   benchmark::run_all<Action>(argc, argv, init);
 }
